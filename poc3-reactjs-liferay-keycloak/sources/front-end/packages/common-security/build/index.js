a8acf993350d60e3830709845d9b77a53429e10f
jpdacunha <dcjeanpaul@gmail.com>
2025-01-28 17:36:11 +0100

    Removing unused files

--- poc3-reactjs-liferay-keycloak/sources/front-end/packages/common-security/build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secureFunction = void 0;
-const secureFunction = () => 'This is a secure function';
-exports.secureFunction = secureFunction;
